4538618a9c4b087881822140a6ad456450298f99
YlianaLisova <lisova229@gmail.com>
2022-08-19 15:12:17 +0300

    search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {IContact} from "../../models/IContact";
 
 @Component({
   selector: 'app-search',
@@ -7,13 +8,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  contactName: string;
+  contacts: IContact[] = [];
   constructor() {
-
+    // const localstorageContacts: IContact[] = JSON.parse(localStorage.getItem('contacts') || '');
+    // this.contacts = localstorageContacts;
+    // console.log(localstorageContacts);
   }
 
   ngOnInit(): void {
   }
 
 
+  getContacts(contactName: string) {
+    const localstorageContacts: IContact[] = JSON.parse(localStorage.getItem('contacts') || '');
+    this.contacts = localstorageContacts;
+    console.log(localstorageContacts);
+    let filteredContacts = this.contacts.filter(con => con.name === contactName);
+    this.contacts = filteredContacts;
+    console.log(filteredContacts)
+  }
 }
 
